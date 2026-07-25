b52ec048f0335b57b5d2687fe17d7eaaa7724dd4
Bandla Bhagya Sri <23bq1a1207@vvit.net>
2026-07-25 22:50:49 +0530

    Time: 24 ms (26.67%), Space: 81.6 MB (80.64%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int left = 0;
+        int right = 1;
+        while (right < nums.length) {
+            if (nums[left] == nums[right])
+                return true;
+
+            left++;
+            right++;
+        }
+        return false;
+    }
+}
